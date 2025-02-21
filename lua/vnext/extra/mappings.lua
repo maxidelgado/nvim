e66e04fbb25fbf19f15bbe38229ac4ef81b516e8
Maximiliano Delgado <maxi@Maximilianos-MacBook-Pro.local>
2025-02-20 23:11:12 -0300

    add custom mappings

---
+++ lua/vnext/extra/mappings.lua
@@ -0,0 +1,10 @@
+local function map(mode, l, r, opts)
+  opts = opts or {}
+  vim.keymap.set(mode, l, r, opts)
+end
+
+map("n", "<C-s>", ":wa<CR>") -- save shortcut
+map("n", "<A-j>", ":m .+1<CR>==") -- move line up(n)
+map("n", "<A-k>", ":m .-2<CR>==") -- move line down(n)
+map("v", "<A-j>", ":m '>+1<CR>gv=gv") -- move line up(v)
+map("v", "<A-k>", ":m '<-2<CR>gv=gv") -- move line down(v)
