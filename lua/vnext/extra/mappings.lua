3fb57c416be8907b0944fcc21feb25b7f2a7ac5e
Maximiliano Delgado <maxi@Maximilianos-MacBook-Pro.local>
2025-02-20 23:18:14 -0300

    improve mappings

--- lua/vnext/extra/mappings.lua
+++ lua/vnext/extra/mappings.lua
@@ -4,6 +4,8 @@ local function map(mode, l, r, opts)
 end
 
 map("n", "<C-s>", ":wa<CR>") -- save shortcut
+map("n", "<A-d>", "yyp") -- duplicate line
+map("v", "<A-d>", ":'<,'>t'><CR>") -- duplicate line
 map("n", "<A-j>", ":m .+1<CR>==") -- move line up(n)
 map("n", "<A-k>", ":m .-2<CR>==") -- move line down(n)
 map("v", "<A-j>", ":m '>+1<CR>gv=gv") -- move line up(v)
